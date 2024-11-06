7d7c729677c6f0e571594b094f381c1a288e5949
Rojen Dangol <rojendangol44@gmail.com>
2024-11-06 09:22:43 +0545

    Tracker update

--- shop/views.py
+++ shop/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from math import ceil
 from .models import Product, Contact, Orders, OrderUpdate
 import json
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
 def index(request):
@@ -24,6 +26,33 @@ def index(request):
     #             [products, range(1, nSlides), nSlides]]
     params = {'allProds': allProds}
     return render(request, "shop/index.html", params)
+
+
+def searchMatch(query, item):
+    # return true only if query matches them item
+    query = query.lower()
+    if query in item.desc.lower() or query in item.product_name.lower() or query in item.category.lower():
+        return True
+    else:
+        return False
+
+def search(request):
+    query = request.GET.get('search')
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prodtemp = Product.objects.filter(category=cat)
+        prod = [item for item in prodtemp if searchMatch(query, item)]
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        if len(prod) != 0:
+            allProds.append([prod, range(1, nSlides), nSlides])
+    params = {'allProds': allProds, 'msg': ""}
+    if len(allProds) == 0 or len(query) < 4:
+        params = {'msg': "Please make sure to enter relevant search query."}
+        # return render(request, "shop/index.html", params)
+    return render(request, "shop/search.html", params)
 
 
 def about(request):
@@ -50,22 +79,18 @@ def tracker(request):
         email = request.POST.get('email', '')
         try:
             order = Orders.objects.filter(order_id=orderId, email=email)
-            if len(order)>0:
-                update= OrderUpdate.objects.filter(order_id=orderId)
+            if len(order) > 0:
+                update = OrderUpdate.objects.filter(order_id=orderId)
                 updates = []
                 for item in update:
                     updates.append({'text': item.update_desc, 'time': item.timestamp})
-                    response = json.dumps([updates, order[0].items_json], default=str)
+                    response = json.dumps({'status': 'success', 'updates': updates, 'itemsJson': order[0].items_json}, default=str)
                 return HttpResponse(response)
             else:
-                return HttpResponse('{}')
+                return HttpResponse('{"status": "No item"}')
         except Exception as e:
-            return HttpResponse('{}')
+            return HttpResponse('{"status": "Error"}')
     return render(request, "shop/tracker.html")
-
-
-def search(request):
-    return render(request, "shop/search.html")
 
 
 def prodView(request,myid):
@@ -78,6 +103,7 @@ def checkout(request):
     if request.method == "POST":
         items_json = request.POST.get('itemsJson', '')
         name = request.POST.get('name', '')
+        amount = request.POST.get('amount', '')
         email = request.POST.get('email', '')
         address = request.POST.get('address1', '') + " " + request.POST.get('address2', '')
         city = request.POST.get('city', '')
@@ -85,12 +111,17 @@ def checkout(request):
         zip_code = request.POST.get('zip_code', '')
         phone = request.POST.get('phone', '')
         order = Orders(items_json=items_json, name=name, email=email, address=address,
-                       city=city, state=state, zip_code=zip_code, phone=phone)
+                       city=city, state=state, zip_code=zip_code, phone=phone, amount=amount)
         order.save()
         update = OrderUpdate(order_id=order.order_id, update_desc="The order has been placed")
         update.save()
         thank = True
         id = order.order_id
         return render(request, "shop/checkout.html", {'thank': thank, 'id':id})
+    #     Request paytm to transfer the amount to your account after paytm by user
     return render(request, "shop/checkout.html")
 
+@csrf_exempt
+def handlerequest(request):
+    # paytm will send you post request here
+    pass
